db29fbafc2b8a503628693d205c8aec77ca6b723
AuPt666 <1491770191@qq.com>
2021-08-03 11:33:51 +0800

    2nd

---
+++ app/src/main/java/com/example/sunnyweatherbykotlin/logic/network/ServiceCreator.kt
@@ -0,0 +1,17 @@
+package com.example.sunnyweatherbykotlin.logic.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceCreator {
+    private const val BASE_URL = "http://api.caiyunapp.com"
+
+    private val retorfit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <T> create(serviceClass: Class<T>): T = retorfit.create(serviceClass)
+
+    inline fun <reified T> create(): T = create(T::class.java)
+}
---
+++ app/src/main/java/com/example/sunnyweatherbykotlin/ui/place/PlaceFragment.kt
@@ -0,0 +1,63 @@
+package com.example.sunnyweatherbykotlin.ui.place
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sunnyweatherbykotlin.R
+
+class PlaceFragment : Fragment(){
+    val viewModel by lazy { ViewModelProviders.of(this).get(PlaceViewModel::class.java) }
+
+    private lateinit var adapter: PlaceAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_place, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManager = LinearLayoutManager(activity)
+        val recyclerView: RecyclerView = requireActivity().findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        recyclerView.adapter = adapter
+        val searchPlaceEdit: EditText = requireActivity().findViewById(R.id.searchPlaceEdit)
+        val bgImageView: ImageView = requireActivity().findViewById(R.id.bgImageView)
+        searchPlaceEdit.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+            } else {
+                recyclerView.visibility = View.GONE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        viewModel.placeLiveData.observe(viewLifecycleOwner, androidx.lifecycle.Observer { result ->
+            val place = result.getOrNull()
+            if (place  != null) {
+                recyclerView.visibility = View.VISIBLE
+                bgImageView.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(place)
+                adapter.notifyDataSetChanged()
+            } else {
+                android.widget.Toast.makeText(activity, "未能查询到任何地方", android.widget.Toast.LENGTH_LONG).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+    }
+}
